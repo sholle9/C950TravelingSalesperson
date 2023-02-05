e4c22d56ac522a6a88b3707f3a6acfd394ea016a
sholle9 <sholle9@wgu.edu>
2023-02-05 12:42:47 -0500

    Address Added csv files added

---
+++ Objects/Address.py
@@ -0,0 +1,26 @@
+import csv
+
+
+class Address:
+
+    def __init__(self, id, address):
+        self._id = id
+        self._address = address
+
+    def __str__(self):
+        return " %s, %s" % (
+            self._id, self._address)
+
+
+def loadAddressData(addressHashTable):
+    fileName = 'C:\dev\SKatuzienski_TaskC950\AddressList.csv'
+    with open(fileName) as addresses:
+        addressData = csv.reader(addresses, delimiter=',')
+        next(addressData)  # skip header
+        for address in addressData:
+            aID = int(address[0])
+            aAddress = str(address[1])
+
+
+            a = Address(aID, aAddress)
+            addressHashTable.insert(aID, a)
